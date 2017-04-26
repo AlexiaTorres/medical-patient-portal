66038163e55a250fe22c21a81e3cde88350141be
Alexia Torres Celorio <R00056@commonms.es>
2017-04-26 17:20:57 +0200

    first patient on json format. Fix on showing model

--- src/app/admin/personal-data/test-form.component.ts
+++ src/app/admin/personal-data/test-form.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FhirClient} from 'ng-fhir/FhirClient';
 
 @Component({
     selector: 'test-form',
@@ -10,36 +11,61 @@ import {Component} from '@angular/core';
 export class TestFormComponent {
     // The schema that will be used to generate a form
     mySchema = {
-     'properties': {
-     'email': {
-     'type': 'string',
-     'description': 'email',
-     'format': 'email'
-     },
-     'password': {
-     'type': 'string',
-     'description': 'Password',
-         'buttons': [{
-             'id': 'reset',
-             'label': 'Reset'
-         }]
-     },
-     'rememberMe': {
-     'type': 'boolean',
-     'default': false,
-     'description': 'Remember me'
-     }
-     },
-     'required': ['email', 'password', 'rememberMe'],
-        'buttons': [{
-            'id': 'alert', // the id of the action callback
-            'label': 'Alert !' // the text inside the button
-        }]
-     };
-
-    myModel = {email: 'john.doe@example.com'};
-    myActions = {
-        'alert': (property) => {alert(JSON.stringify(property.value))},
-        'reset': (property) => {property.reset()}
+        properties: {
+            fullUrl: 'http://fhirtest.uhn.ca/baseDstu2/Patient/27944',
+            resource: {
+                resourceType: 'Patient',
+                id: '27944',
+                meta: {
+                    versionId: '4'
+                },
+                lastUpdated: '2017-04-23T23:05:15.587-04:00'
+            },
+            text: {
+                status: 'generated',
+                div: '<div xmlns="http://www.w3.org/1999/xhtml">SANTIAGO KUROWSKI</div>'
+            },
+            name: [{
+                use: 'official',
+                text: 'SANTIAGO KUROWSKI',
+                family: ['KUROWSKI'],
+                given: ['SANTIAGO']
+            }],
+            gender: 'female',
+            birthDate: '1937-05-14',
+            search: {'mode': 'match'}
+        }
     };
+    myActions = {
+        'alert': (property) => {
+            alert(JSON.stringify(property.value));
+        },
+        'reset': (property) => {
+            property.reset();
+        }
+    };
+// Fhir
+    private client: FhirClient;
+
+    private config: any = {
+        'baseUrl': 'http://fhirtest.uhn.ca/baseDstu2',
+        'credentials': 'same-origin',
+    };
+    public patient: any = {};
+
+    constructor() {
+
+        this.client = new FhirClient(this.config);
+        this.client.search({type: 'Patient', query: {}}).then((response) => {
+            if (response.data) {
+                this.patient = (response.data.entry[0]);
+            }
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    myStringify(obj: any): string {
+        return JSON.stringify(obj, null, '  ');
+    }
 }
--- src/app/admin/personal-data/test-form.module.ts
+++ src/app/admin/personal-data/test-form.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SchemaFormModule, WidgetRegistry, DefaultWidgetRegistry } from 'angular2-schema-form';
 import { TestFormComponent } from './test-form.component';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
     imports: [
         SchemaFormModule,
-        BrowserModule
+        BrowserModule,
+        HttpModule
     ],
     declarations: [TestFormComponent],
     providers: [{provide: WidgetRegistry, useClass: DefaultWidgetRegistry}]
