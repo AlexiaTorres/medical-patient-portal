67e67ee9cc097235b132cbf3175c69a358c0c297
Alexia Torres Celorio <R00056@commonms.es>
2017-05-29 12:34:52 +0200

    test fhir component

--- src/app/admin/personal-data/test-form.component.ts
+++ src/app/admin/personal-data/test-form.component.ts
@@ -15,7 +15,6 @@ export class TestFormComponent {
             fullUrl: {
                 'type': 'string',
                 'description': 'url'
-                
             },
             resource: {
                 'type': 'object',
